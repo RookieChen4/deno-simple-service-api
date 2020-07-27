4c3aaa313d2ce2a161ef1c9e95ad443e0fe8b8bb
cowboy bebop <1164566601@qq.com>
2020-07-27 19:07:45 +0800

    模板渲染

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
 import { Application, Router, send } from "https://deno.land/x/oak@v4.0.0/mod.ts"
 import router from './routes.ts'
 import { handleAuthHeader, handleErrors } from "./middlewares.ts"
+import * as dejs from 'https://deno.land/x/dejs@0.8.0/mod.ts';
+import { renderFileToString } from 'https://deno.land/x/dejs/mod.ts';
 import User from "./interface/user.ts";
+const { cwd, stdout, copy } = Deno;
 const port = Deno.env.get("PORT") || 5000
 // const app = new Application()
 const app = new Application<{
@@ -19,6 +22,13 @@ app.use(async (ctx: any, next: any) => {
 app.use(async (context,next: () => Promise<void>) => {
   if(context.request.url.pathname.includes('api')) {
     return await next()
+  }
+  if(context.request.url.pathname.includes('template')) {
+    const output = await renderFileToString(`${cwd()}/views/template.ejs`, {
+      name: 'world',
+    });
+    context.response.body = output
+    return
   }
   await send(context, context.request.url.pathname, {
     root: `${Deno.cwd()}/views`,
