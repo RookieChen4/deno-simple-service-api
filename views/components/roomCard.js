9d1cddc0caa6a38e5b0ee603a17d5ab30f661760
cowboy bebop <1164566601@qq.com>
2020-07-30 00:22:37 +0800

    首页更新 添加组件

---
+++ views/components/roomCard.js
@@ -0,0 +1,41 @@
+// @ts-nocheck
+class roomCard extends HTMLElement {
+    shadow;
+    article = document.createElement('article')
+    style = document.createElement('style')
+    a = document.createElement('a')
+    img = document.createElement('img')
+    p = document.createElement('p')
+
+    constructor() {
+        super()
+        this.shadow = this.attachShadow({mode: 'open'})
+        this.img.src = "./assets/article.png"
+        this.p.innerHTML = '名称'
+        this.a.appendChild(this.img)
+        this.a.appendChild(this.p)
+        this.article.appendChild(this.a)
+        this.shadow.appendChild(this.article)
+        this.style.textContent = `
+            article {
+                width: 100%;
+            }        
+            a {
+                width: 100%;
+            }
+            img {
+                width: 100%;
+                height: auto;
+            }
+            
+            p {
+                text-align: center;
+            }
+        `;
+        this.shadow.appendChild(this.style)
+    }
+}
+
+
+customElements.define('room-card', roomCard)
+  
